a72334a545e0551cbdc5ed031d7f136cc88e0d43
Hans Foster <fosterhans@gmail.com>
2019-10-24 21:32:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,12 @@ def oxford_comma(array)
   last_word = array.last
   array.pop
   final_string = array.join(", ")
-  final_string += " ,and " + last_word
-  final_string
+  if (array.size > 2)
+    final_string += ", and " + last_word
+  elsif (array.size == 2)
+    final_string += " and " + last_word
+  else
+    final_string = last_word
+  end
+    final_string
 end
