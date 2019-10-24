bed36f2ae275a59bea5547c05f48036a6595096e
Hans Foster <fosterhans@gmail.com>
2019-10-24 21:29:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   last_word = array.last
   array.pop
   final_string = array.join(", ")
-  final_string += "and " + 
-  
+  final_string += "and " + last_word
+  final_string
 end
