10202f76714f0681d7ced52f3a402884e6142d23
Hans Foster <fosterhans@gmail.com>
2019-10-24 21:33:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  last_word = array.last
+  array.pop
+  final_string = array.join(", ")
+  if (array.size > 1)
+    final_string += ", and " + last_word
+  elsif (array.size == 1)
+    final_string += " and " + last_word
+  else
+    final_string = last_word
+  end
+    final_string
 end
